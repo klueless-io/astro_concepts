b23221a9899b67962fd4c9644abacb1453a476c4
David Cruwys <david@ideasmen.com.au>
2022-09-24 08:56:01 +1000

    first commit

---
+++ lib/astro_concepts.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "astro_concepts/version"
+
+module AstroConcepts
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/astro_concepts/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module AstroConcepts
+  VERSION = "0.1.0"
+end
